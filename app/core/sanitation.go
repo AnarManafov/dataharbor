4179f04047e7e4b987b37c6f424bfdc9072d60bc
Anar Manafov <Anar.Manafov@gmail.com>
2024-08-25 12:41:24 +0200

    Call SanitationJob before the first tick

--- app/core/sanitation.go
+++ app/core/sanitation.go
@@ -24,34 +24,41 @@ func NewSanitationScheduler() (ticker *time.Ticker, done chan bool) {
 	return ticker, done
 }
 
+func CleanStagingDir() {
+	files, err := os.ReadDir(common.XrdConfig.StagingPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			common.Logger.Info("Check staging dir: " + file.Name())
+			file_inf, err := file.Info()
+			if err == nil && strings.HasPrefix(file.Name(), common.XrdConfig.StagingTmpDirPrefix) && IsOlderThanXHours(file_inf.ModTime(), 2) {
+				dirToRemove := path.Join(common.XrdConfig.StagingPath, file.Name())
+				common.Logger.Info("Need to remove staging dir: " + dirToRemove)
+				if err := os.RemoveAll(dirToRemove); err != nil {
+					common.Logger.Error(err)
+				} else {
+					common.Logger.Info("Removed staging dir: " + dirToRemove)
+				}
+			}
+		}
+	}
+}
+
 func SanitationJob(ticker *time.Ticker, done chan bool) {
+	// Since the Tick is not called immediately, we force the clean at the start
+	CleanStagingDir()
+
 	for {
 		select {
 		case <-done:
-			common.Logger.Info("DBG: Sanitation check job - done")
+			common.Logger.Info("Sanitation check job - done")
 			return
 		case t := <-ticker.C:
 			common.Logger.Info("Sanitation check of staging dir at ", t)
-			files, err := os.ReadDir(common.XrdConfig.StagingPath)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			for _, file := range files {
-				if file.IsDir() {
-					common.Logger.Info("Check staging dir: " + file.Name())
-					file_inf, err := file.Info()
-					if err == nil && strings.HasPrefix(file.Name(), common.XrdConfig.StagingTmpDirPrefix) && IsOlderThanXHours(file_inf.ModTime(), 2) {
-						dirToRemove := path.Join(common.XrdConfig.StagingPath, file.Name())
-						common.Logger.Info("Need to remove staging dir: " + dirToRemove)
-						if err := os.RemoveAll(dirToRemove); err != nil {
-							common.Logger.Error(err)
-						} else {
-							common.Logger.Info("Removed staging dir: " + dirToRemove)
-						}
-					}
-				}
-			}
+			CleanStagingDir()
 		}
 	}
 }
