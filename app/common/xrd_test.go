2e5bdb8f52feecda560eb1bbebdb7bc367b1a36d
Anar Manafov <Anar.Manafov@gmail.com>
2024-09-17 13:02:10 +0200

    Add unit tests

---
+++ app/common/xrd_test.go
@@ -0,0 +1,32 @@
+package common
+
+import (
+	"testing"
+
+	"github.com/spf13/viper"
+)
+
+func resetAndParseXrdConfig() {
+	viper.Reset()
+	ParseXrdConfig()
+}
+
+func TestParseXrdConfig(t *testing.T) {
+	resetAndParseXrdConfig()
+
+	tests := []struct {
+		field    string
+		expected interface{}
+		actual   interface{}
+	}{
+		{"initial_ir", "/tmp/", XrdConfig.InitialDir},
+		{"host", "localhost", XrdConfig.Host},
+		{"process_timeout", 30, int(XrdConfig.ProcessTimeout)},
+	}
+
+	for _, tt := range tests {
+		if tt.actual != tt.expected {
+			t.Errorf("Expected %s to be %v, but got %v", tt.field, tt.expected, tt.actual)
+		}
+	}
+}
