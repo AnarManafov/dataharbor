f092d8041a1540215b9bc540fe94519fe711e55a
Anar Manafov <Anar.Manafov@gmail.com>
2024-09-30 13:58:05 +0200

    Add a rout for Auth token

---
+++ web/src/components/partials/CallbackComponent.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    created() {
+        const token = this.$route.query.token;
+        if (token) {
+            // Store the token securely
+            localStorage.setItem('authToken', token);
+            console.log('Token: ', token);
+            
+            // Redirect to the main app
+            this.$router.push('/browse');
+        } else {
+            // Handle error
+            console.error('No token found in URL');
+        }
+    }
+}
+</script>
